86007aa74c9bff1756106594f8f46e84e8b608fa
Daniel Vega <dani.vega.aguirre@gmail.com>
2019-11-14 13:20:36 -0400

    14/11

---
+++ busqueda.php
@@ -0,0 +1,31 @@
+<?php
+include("header.php");
+?>
+     <div class="card card-body" id="top">
+            <form action="save_task.php" method="POST">
+            
+                <div class="form-group">
+                    <input type="text" name="title" class="form-control" placeholder="Task Title" autofocus>
+                </div>    
+                <div class="form-group">
+                    <inp   name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+                </div>
+                <input type="submit" class="btn btn-success btn-block"
+                name="save_task" placeholder="Save Task">
+
+
+            </form>
+        </div>
+    
+
+
+
+        <footer>
+        <div id="footerText">Contactos <hr> Daniel Marcelo Vega Aguirre<br>Victor Ochoa <br>Aron Ramirez</div>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/d92523541a.js" crossorigin="anonymous"></script>
+    </footer>
+</body>
+</html>
--- header.php
+++ header.php
@@ -9,6 +9,7 @@ include("conexion.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/estilos.css">
     <link rel="stylesheet" href="CSS/styles.css">
     <title>INFINITY</title>
@@ -36,7 +37,7 @@ include("conexion.php");
 					<div class="dropOut">
 						<div class="triangle"></div>
 						<ul>
-							<ol> <a href=""> Busqueda </a> </ol>
+							<ol> <a href="busqueda.php"> Busqueda </a> </ol>
 							<ol><a href="">Administrar</a></ol>
 							<ol><a href="">Algo </a></ol>
 							
