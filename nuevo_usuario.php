4e8389181e728184542a621887c41617108f5786
Daniel Vega <dani.vega.aguirre@gmail.com>
2019-11-18 16:04:57 -0400

    Ya termine la parte de crear nuevos clientes con usuarios xD

--- nuevo_usuario.php
+++ nuevo_usuario.php
@@ -11,19 +11,20 @@ include("header.php");
      <h1><br>&bull; Nuevo Usuario &bull;</h1>
      <form name="formulario" method="POST" action="registro.php" class="mx-auto row px-md-5">
           <div class="row">
+          
+          <!-- Primer Nombre -->          
           <?php 
-            if (isset($_SESSION['message']))
+            if (isset($_SESSION['nombre1']))
             {
                 ?>
                <div class="col">
                <div class="input-group mb-3">
                <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
-            <?= $_SESSION['message'] ?>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        </div>
-
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
                </div>  
                </div>
                <div class="w-100"></div>
@@ -32,32 +33,177 @@ include("header.php");
         ?>
                <div class="col">
                <div class="input-group mb-3">
-                    <input type="box" name="nombre" class="form-control" placeholder="Usuario" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <input type="box" name="nombre1" class="form-control" placeholder="Primer Nombre" aria-label="Recipient's username" aria-describedby="button-addon2" required>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+
+          <!-- Segundo Nombre-->          
+          <?php 
+            if (isset($_SESSION['nombre2']))
+            {
+                ?>
+               <div class="col">
+               <div class="input-group mb-3">
+               <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+               <?php 
+           session_unset(); }
+        ?>
+               <div class="col">
+               <div class="input-group mb-3">
+                    <input type="box" name="nombre2" class="form-control" placeholder="Segundo Nombre" aria-label="Recipient's username" aria-describedby="button-addon2">
                </div>  
                </div>
                <div class="w-100"></div>
 
 
+          <!-- Ap Paterno-->          
+          <?php 
+            if (isset($_SESSION['ap_pat']))
+            {
+                ?>
+               <div class="col">
+               <div class="input-group mb-3">
+               <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+               <?php 
+           session_unset(); }
+        ?>
+               <div class="col">
+               <div class="input-group mb-3">
+                    <input type="box" name="ap_pat" class="form-control" placeholder="Apellido Paterno" aria-label="Recipient's username" aria-describedby="button-addon2"required>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+
+
+          <!-- Apellido Materno-->          
+          <?php 
+            if (isset($_SESSION['ap_mat']))
+            {
+                ?>
+               <div class="col">
+               <div class="input-group mb-3">
+               <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+               <?php 
+           session_unset(); }
+        ?>
+               <div class="col">
+               <div class="input-group mb-3">
+                    <input type="box" name="ap_mat" class="form-control" placeholder="Apellido Materno" aria-label="Recipient's username" aria-describedby="button-addon2"required>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+
+
+          <!-- Ci Nombre-->          
+          <?php 
+            if (isset($_SESSION['ci']))
+            {
+                ?>
+               <div class="col">
+               <div class="input-group mb-3">
+               <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+               <?php 
+           session_unset(); }
+        ?>
+               <div class="col">
+               <div class="input-group mb-3">
+                    <input type="box" name="ci" class="form-control" placeholder="CI" aria-label="Recipient's username" aria-describedby="button-addon2"required>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+
+ <!-- USUARIO-->          
+ <?php 
+            if (isset($_SESSION['usuario']))
+            {
+                ?>
+               <div class="col">
+               <div class="input-group mb-3">
+               <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+               <?php 
+           session_unset(); }
+        ?>
+               <div class="col">
+               <div class="input-group mb-3">
+                    <input type="text" name="usuario" class="form-control" placeholder="Usuario" title="Letras y números. Tamaño mínimo: 5. Tamaño máximo: 40" aria-label="Recipient's username" aria-describedby="button-addon2"minlength="5" maxlength="40" required pattern="[A-Za-z0-9]+" required>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+ <!--PASS-->          
+ <?php 
+            if (isset($_SESSION['pass']))
+            {
+                ?>
+               <div class="col">
+               <div class="input-group mb-3">
+               <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show" role="alert">
+               <?= $_SESSION['message'] ?>
+               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+               <span aria-hidden="true">&times;</span>
+               </button>
+               </div>
+               </div>  
+               </div>
+               <div class="w-100"></div>
+               <?php 
+           session_unset(); }
+        ?>
+               <div class="col">
+               <div class="input-group mb-3">
+                    <input type="password" name="pass" class="form-control" placeholder="Contraseña" aria-label="Recipient's username" aria-describedby="button-addon2" required>
+               </div>  
+               </div>
+               <div class="w-100"></div>
 
 
                <div class="col">
                <div class="input-group mb-3">
-                    <input type="password" 
-                    name="pass" class="form-control" placeholder="Contraseña" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <input type="submit" class="btn btn-warning btn-lg btn-block" value="Registrar Cuenta" aria-label="Crear Cuenta" aria-describedby="button-addon2" name="registro">
                </div>  
                </div>
                <div class="w-100"></div>
-               <div class="col">
-               <div class="input-group mb-3">
-                    <input type="submit" class="btn btn-primary btn-lg btn-block" placeholder="Recipient's username" aria-label="Entrar" aria-describedby="button-addon2">
-               </div>  
-               </div>
-               <div class="w-100"></div>
-               <div class="col">
-               <div class="input-group mb-3">
-                    <a class=" btn btn-secondary btn-lg btn-block" href="nuevo_usuario.php">Registrate Ya!</a>
-               </div>  
-               </div>
+              
           </div>
      </form>
 </div>
--- registro.php
+++ registro.php
@@ -1,26 +1,67 @@
 <?php
 include("conexion.php");
-if(isset($_POST['registro']))
+if(isset($_POST['usuario']))
 {
 $usuario = $_POST['usuario'];
 $pass = $_POST['pass'];
 $ci = $_POST['ci'];
-$nombre = $_POST['nombre'];
+$nombre = $_POST['nombre1'];
 $nombre2 = $_POST['nombre2'];
 $ap_pat = $_POST['ap_pat'];
 $ap_mat = $_POST['ap_mat'];
-$query = "SELECT cliente.nombre AS 'nonre' FROM cliente JOIN cuentas ON cliente.id_cuenta = cuentas.id_usuario WHERE cuentas.usuario = '$usuario' OR cuentas.contraseña = '$pass'";
+#echo $usuario,$pass,$ci,$nombre,$nombre2,$ap_mat,$ap_pat;
+$query = "SELECT cuentas.usuario AS 'nonre' FROM cuentas WHERE cuentas.usuario = '$usuario' ";
 $res = mysqli_query($conn,$query);
- if(isset($res))
+$row = mysqli_fetch_array($res);
+$us=$row['nonre'];
+
+#Aqui veo si existe algun usuario registrado con el nuevo usuario
+#si existiera le mando una alerta
+ if($us == $usuario)
  {
-    header ('Location: nuevo_usuario.php') ;
+   $_SESSION['usuario'] = $usuario; 
+   $_SESSION['message'] = "El Usuario '$usuario' ya se encuentra registrado, intente uno diferente";
+   $_SESSION['message_type'] = "danger";
+   header ('Location: nuevo_usuario.php') ;
  }
+ else #si no existe verifico si existe el cliente en la base de datos para no repetirlo
+ {
+   $query="SELECT cliente.id_cliente AS 'id' FROM cliente WHERE cliente.nombre = '$nombre' AND cliente.segundoNombre = '$nombre2'
+   AND cliente.ap_paterno = '$ap_pat' AND cliente.ap_materno = '$ap_mat'";
+   $res = mysqli_query($conn,$query);
+      if(mysqli_num_rows($res)!=0)
+   {  #tambien mando una alerta
+      $_SESSION['nombre1'] = $nombre.$nombre2.$ap_pat.$ap_mat; 
+      $_SESSION['message'] = "El Cliente '$nombre $nombre2 $ap_pat $ap_mat' ya tiene una cuenta de usuario, intente uno diferente o contacte a un Administrador";
+      $_SESSION['message_type'] = "danger";
+      header ('Location: nuevo_usuario.php') ;
+   }
+   else#si no existiera nada igual, puedo hacer un nuevo registo
+   {
+      $query="INSERT INTO `cuentas` (`id_usuario`, `usuario`, `contraseña`, `tipo`) VALUES (NULL, '$usuario', '$pass', '1')";
+      $res = mysqli_query($conn,$query);
+
+      #el primer registro creado es en la tabla cuentas, luego en la de clientes
+      #pero necesito el id con el que se creo este nuevo registro para casasr los registros
+      $query="SELECT cuentas.id_usuario AS 'id' FROM cuentas WHERE cuentas.usuario = '$usuario' AND cuentas.contraseña = '$pass'";
+      $res=mysqli_query($conn,$query);
+      $row = mysqli_fetch_array($res);
+      $id=$row['id'];
+      $query= "INSERT INTO `cliente` (`id_cliente`, `nombre`, `segundoNombre`, `ap_paterno`, `ap_materno`, `ci`, `id_cuenta`) VALUES (NULL, '$nombre', '$nombre2', '$ap_pat', '$ap_mat', '$ci', '$id')";
+      $res=mysqli_query($conn,$query);
+      if(isset($res))
+      {
+         header ('Location: LobiLogeo.php') ;
+      }
+   
+   }  
 
 
+ }
 }
 else
 {
-    
+  echo "no me llego naranjas;"  ;
 }
 
 
